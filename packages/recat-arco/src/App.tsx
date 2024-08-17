9ab4cdda3e870514ee7d72f758d3ef72a1dd0c57
wzy <wzyzzqq@163.com>
2024-08-17 16:59:24 +0800

    初始化react-arco项目并使用arco design组件

---
+++ packages/recat-arco/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import { Button } from "@arco-design/web-react";
+function App() {
+  return (
+    <>
+      <Button type="primary">Button</Button>
+    </>
+  );
+}
+
+export default App;
